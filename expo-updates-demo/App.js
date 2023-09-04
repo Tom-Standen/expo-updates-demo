6d445c1cfe94454b011320d25a88f3d3d6d4a738
Tom Standen <tomstanden@Toms-MacBook-Pro.local>
2023-09-04 12:35:08 +0100

    Added env variables to eas.json

--- expo-updates-demo/App.js
+++ expo-updates-demo/App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const onboardingEndpoint = process.env.EXPO_PUBLIC_ONBOARDING_SERVICE_ENDPOINT
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is an update</Text>
+      <Text>Onboarding endpoint: {onboardingEndpoint}</Text>
       <StatusBar style="auto" />
     </View>
   );
